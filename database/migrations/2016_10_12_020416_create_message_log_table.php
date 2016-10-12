847f766e02bc756d4a165d5219c5647055667508
Jem Raymundo <jeremuel.raymundo@gqaustralia.edu.au>
2016-10-12 02:24:19 +0000

    Create migration for Message Logs
    Resolves: SI-23

---
+++ database/migrations/2016_10_12_020416_create_message_log_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMessageLogTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('message_log', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('sent_message_id');
+            $table->string('result');
+            $table->text('response_body');
+            $table->timestamp('created_at');
+            $table->timestamp('updated_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('message_log');
+    }
+}
